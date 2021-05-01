3c15b0e782e30c173201d8fa0063f8d60054c5b0
Sheharyar Naseer <hello@sheharyar.me>
2021-05-01 09:27:56 +0500

    Set minimum elixir version to 1.10.x

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SafeURL.MixProject do
     [
       app: :safeurl,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
