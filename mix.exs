ac087d6a1385e73151bfbf301b38e492d1022fce
Sheharyar Naseer <hello@sheharyar.me>
2022-01-31 18:56:21 -0800

    Prepare for publishing

--- mix.exs
+++ mix.exs
@@ -1,19 +1,47 @@
 defmodule SafeURL.MixProject do
   use Mix.Project
 
+
+  @app     :safeurl
+  @name    "SafeURL"
+  @version "0.1.0"
+  @github  "https://github.com/slab/safeurl-elixir"
+
+
   def project do
     [
-      app: :safeurl,
-      version: "0.1.0",
-      elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      # Project
+      app:            @app,
+      version:        @version,
+      elixir:         "~> 1.10",
+      description:    description(),
+      package:        package(),
+      deps:           deps(),
+      elixirc_paths:  elixirc_paths(Mix.env()),
+
+      # ExDoc
+      name:           @name,
+      docs: [
+        main:         @name,
+        source_url:   @github,
+        homepage_url: @github,
+        canonical:    "https://hexdocs.pm/#{@app}",
+        extras:       ["README.md"]
+      ]
     ]
   end
 
+
+  defp description do
+    "Elixir Implementation of SafeURL, Anti-SSRF"
+  end
+
+
+  # BEAM Application
   def application do
     [env: default_configs()]
   end
+
 
   defp default_configs do
     [
@@ -24,11 +52,34 @@ defmodule SafeURL.MixProject do
     ]
   end
 
+
+  # Dependencies
   defp deps do
     [
       {:httpoison, "~> 1.8"},
       {:inet_cidr, "~> 1.0"},
       {:dns, "~> 2.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+
+  # Compilation Paths
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
+
+  # Package Information
+  defp package do
+    [
+      name: @app,
+      maintainers: ["Sheharyar Naseer", "Include Security", "Jason Chen"],
+      licenses: ["MIT"],
+      files: ~w(mix.exs lib README.md),
+      links: %{
+        "Github" => @github,
+        "Slab" => "https://slab.com/",
+      }
     ]
   end
 end
