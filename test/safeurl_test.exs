1d831e440b9a79e8f90911bf715b824dce8e404d
Bo H <bhan@slab.com>
2022-05-05 14:12:17 +0800

    mock dns

--- test/safeurl_test.exs
+++ test/safeurl_test.exs
@@ -1,6 +1,8 @@
 defmodule SafeURLTest do
   use ExUnit.Case
 
+  import Mock
+
   # setup_all do
   #   global_whitelist = ["10.0.0.0/24"]
   #   global_blacklist = ["8.8.0.0/16"]
@@ -10,12 +12,14 @@ defmodule SafeURLTest do
 
   describe "#allowed?" do
     test "returns true for only allowed schemes" do
-      assert SafeURL.allowed?("http://includesecurity.com")
-      assert SafeURL.allowed?("https://includesecurity.com")
-      refute SafeURL.allowed?("ftp://includesecurity.com")
+      with_mock DNS, [resolve: fn(_hostname) -> {:ok, [{192, 0, 78, 24}]} end] do
+        assert SafeURL.allowed?("http://includesecurity.com")
+        assert SafeURL.allowed?("https://includesecurity.com")
+        refute SafeURL.allowed?("ftp://includesecurity.com")
 
-      assert SafeURL.allowed?("ftp://includesecurity.com", schemes: ~w[ftp])
-      refute SafeURL.allowed?("http://includesecurity.com", schemes: ~w[ftp])
+        assert SafeURL.allowed?("ftp://includesecurity.com", schemes: ~w[ftp])
+        refute SafeURL.allowed?("http://includesecurity.com", schemes: ~w[ftp])
+      end
     end
 
     test "returns false for reserved ranges" do
@@ -39,7 +43,9 @@ defmodule SafeURLTest do
     test "blocking custom IP ranges" do
       opts = [blocklist: ["5.5.0.0/16", "100.0.0.0/24"]]
 
-      assert SafeURL.allowed?("http://includesecurity.com", opts)
+      with_mock DNS, [resolve: fn(_hostname) -> {:ok, [{192, 0, 78, 24}]} end] do
+        assert SafeURL.allowed?("http://includesecurity.com", opts)
+      end
       assert SafeURL.allowed?("http://3.3.3.3", opts)
       refute SafeURL.allowed?("http://5.5.5.5", opts)
       refute SafeURL.allowed?("http://100.0.0.50", opts)
@@ -50,7 +56,9 @@ defmodule SafeURLTest do
 
       assert SafeURL.allowed?("http://10.0.0.1/", opts)
       refute SafeURL.allowed?("http://72.254.45.178", opts)
-      refute SafeURL.allowed?("https://includesecurity.com", opts)
+      with_mock DNS, [resolve: fn(_hostname) -> {:ok, [{192, 0, 78, 24}]} end] do
+        refute SafeURL.allowed?("https://includesecurity.com", opts)
+      end
     end
   end
 end
