70c3e3563de1af1d968d439249ba0fef6c98448d
Bo H <bhan@slab.com>
2022-05-10 14:38:23 +0800

    implement TestDNSResolver

--- test/safeurl_test.exs
+++ test/safeurl_test.exs
@@ -1,3 +1,10 @@
+defmodule TestDNSResolver do
+  @behaviour DNSResolver
+
+  @impl DNSResolver
+  def resolve(_domain), do: {:ok, [{192, 0, 78, 24}]}
+end
+
 defmodule SafeURLTest do
   use ExUnit.Case
 
@@ -10,12 +17,12 @@ defmodule SafeURLTest do
 
   describe "#allowed?" do
     test "returns true for only allowed schemes" do
-      assert SafeURL.allowed?("http://includesecurity.com")
-      assert SafeURL.allowed?("https://includesecurity.com")
-      refute SafeURL.allowed?("ftp://includesecurity.com")
+      assert SafeURL.allowed?("http://includesecurity.com", dns_module: TestDNSResolver)
+      assert SafeURL.allowed?("https://includesecurity.com", dns_module: TestDNSResolver)
+      refute SafeURL.allowed?("ftp://includesecurity.com", dns_module: TestDNSResolver)
 
-      assert SafeURL.allowed?("ftp://includesecurity.com", schemes: ~w[ftp])
-      refute SafeURL.allowed?("http://includesecurity.com", schemes: ~w[ftp])
+      assert SafeURL.allowed?("ftp://includesecurity.com", schemes: ~w[ftp], dns_module: TestDNSResolver)
+      refute SafeURL.allowed?("http://includesecurity.com", schemes: ~w[ftp], dns_module: TestDNSResolver)
     end
 
     test "returns false for reserved ranges" do
@@ -37,7 +44,7 @@ defmodule SafeURLTest do
     end
 
     test "blocking custom IP ranges" do
-      opts = [blocklist: ["5.5.0.0/16", "100.0.0.0/24"]]
+      opts = [blocklist: ["5.5.0.0/16", "100.0.0.0/24"], dns_module: TestDNSResolver]
 
       assert SafeURL.allowed?("http://includesecurity.com", opts)
       assert SafeURL.allowed?("http://3.3.3.3", opts)
@@ -46,7 +53,7 @@ defmodule SafeURLTest do
     end
 
     test "only allows IPs in the allowlist when present" do
-      opts = [allowlist: ["10.0.0.0/24"]]
+      opts = [allowlist: ["10.0.0.0/24"], dns_module: TestDNSResolver]
 
       assert SafeURL.allowed?("http://10.0.0.1/", opts)
       refute SafeURL.allowed?("http://72.254.45.178", opts)
